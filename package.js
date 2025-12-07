49f7dd91cbcb9a0ce7ece804d456beac59356689
statco <39243573+statco@users.noreply.github.com>
2025-12-07 17:34:10 -0500

    Update package.js

--- package.js
+++ package.js
@@ -2,10 +2,12 @@
   "name": "gci-brain",
   "version": "1.0.0",
   "type": "module",
+  "description": "Middleware for GCI Consensus Engine",
   "dependencies": {
-    "node-fetch": "^3.3.2"
+    "node-fetch": "^3.3.2"  // <--- ADD THIS LINE
   }
 }
+
 
 
 
