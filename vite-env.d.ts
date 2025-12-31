867e3e38a290df1088c38e2d5f743bb2e289bb22
statco <39243573+statco@users.noreply.github.com>
2025-12-31 11:52:53 -0500

    Add VITE_GOOGLE_MAPS_API_KEY to environment variables

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -13,6 +13,7 @@ interface ImportMetaEnv {
   readonly VITE_AIRTABLE_PAYMENTS_TABLE: string
   readonly VITE_AIRTABLE_NOTIFICATIONS_TABLE: string
   readonly VITE_WHEEL_SIZE_API_KEY: string
+  readonly VITE_GOOGLE_MAPS_API_KEY: string
 }
 
 interface ImportMeta {
